4314b06f1d8034397623f2d2c4e6c8767c9e62b8
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-05-13 22:12:35 +0800

    main

--- resources/views/admin/contents/dashboard/index.blade.php
+++ resources/views/admin/contents/dashboard/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="pagetitle">
-    <h1>Blank Page</h1>
+    <h1>Dashboard</h1>
     <nav>
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item"><a href="/admin/dashboard">Home</a></li>
         <li class="breadcrumb-item">Pages</li>
         <li class="breadcrumb-item active">Blank</li>
       </ol>
