5c6abfd7ffa1eacf31a478f60cff1b8d4d87da80
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-05-13 23:06:55 +0800

    main

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
-use illuminate\Support\Facades\View;
+// use illuminate\Support\Facades\View;
 
 class AdminController extends Controller
 {
     public function index(){
-        $data_layout = [
-            'title' => 'Halaman admin',
-            'card_title' => 'Selamat Datang Di halaman Admin',
-        ];
-        view::share($data_layout);
-        return view('admin.index', []);
+        // $data_layout = [
+        //     'title' => 'Halaman admin',
+        //     'card_title' => 'Selamat Datang Di halaman Admin',
+        // ];
+        // view::share($data_layout);
+        return view('admin/index', []);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,5 @@
 <x-layout>
+    <x-slot name='title'>Halaman admin</x-slot>
+    <x-slot name='card_title'>Selamat Datang Di halaman Admin</x-slot>
     <h1>Welcome to Admin Page</h1>
 </x-layout>
