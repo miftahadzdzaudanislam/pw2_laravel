93c0e9131eb28043352a20f95046dfc4ce9b21e5
miftahadzdzaudanislam <miftahdzaudan@gmail.com>
2024-06-12 19:34:32 +0800

    tugas 13

--- resources/views/admin/partials/_head.blade.php
+++ resources/views/admin/partials/_head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
   
-    <title>Components / Accordion - NiceAdmin Bootstrap Template</title>
+    <title>eLearning | Admin</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
   
--- resources/views/admin/partials/_header.blade.php
+++ resources/views/admin/partials/_header.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="{{asset('NiceAdmin')}}/assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">NiceAdmin</span>
+        <span class="d-none d-lg-block">eLearning</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
